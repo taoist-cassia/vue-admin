ff1f8bf8ce3e23617e59af1b7ba5f2fdcf0a5670
qingjueaaa@163.com <qingjueaaa@163.com>
2020-07-24 18:00:23 +0800

    first-commit

---
+++ src/views/Login/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="login">
+        登录页面
+        <el-button>默认按钮</el-button>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'login'
+    }
+</script>
+
+<style scoped lang="scss">
+#login{
+    height: 100vh;
+    background-color: #344a5f;
+}
+</style>
