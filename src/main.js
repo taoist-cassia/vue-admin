4b8a6a6c07a62853f23fc02a6737f325f1f5b688
qingjueaaa@163.com <qingjueaaa@163.com>
2020-07-28 17:50:42 +0800

    数据对接

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from "vue";
+import VueCompositionApi from '@vue/composition-api';
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
+
 Vue.use(ElementUI);
+Vue.use(VueCompositionApi);
 
 Vue.config.productionTip = false;
 
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -15,6 +15,10 @@
                     <label>密码</label>
                     <el-input type="password" v-model="ruleForm.password" autocomplete="off" minlength="6" maxlength="20"></el-input>
                 </el-form-item>
+                <el-form-item prop="passwords" class="item-form" v-show="model === 'register'">
+                    <label>重复密码</label>
+                    <el-input type="password" v-model="ruleForm.passwords" autocomplete="off" minlength="6" maxlength="20"></el-input>
+                </el-form-item>
                 <el-form-item prop="code" class="item-form">
                     <label>验证码</label>
                     <el-row :gutter="20">
@@ -31,86 +35,137 @@
 </template>
 
 <script>
+    import { reactive, ref, isRef, toRef, onMounted  } from '@vue/composition-api';
+    import { stripscript, validateEmail, validatePass, validateVCode } from '@/utils/validate';
     export default{
         name: 'login',
-        data(){
+        // setup(props,context){
+        setup(props,{ refs }){
+            //这里面放置data数据，生命周期，自定义的函数
+
             //验证用户名
-            var validateUsername = (rule, value, callback) => {
-                let reg = /^([a-zA-z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/;
+            let validateUsername = (rule, value, callback) => {
                 if (value === '') {
                     return callback(new Error('请输入用户名(邮箱)'));
-                }else if(!reg.test(value)){
+                }else if(validateEmail(value)){
                     return callback(new Error('用户名格式有误'));
                 }else {
                     callback();
                 }
             };
             //验证密码
-            var validatePassword = (rule, value, callback) => {
-                let reg = /^(?!\D+$)(?![^a-zA-Z]+$)\S{6,20}$/;
+            let validatePassword = (rule, value, callback) => {
+                //过滤后的数据
+                ruleForm.password = stripscript(value);
+                value =ruleForm.password;
+
                 if (value === '') {
                     callback(new Error('请输入密码'));
-                }else if(!reg.test(value)){
+                }else if(validatePass(value)){
                     return callback(new Error('密码为6至20位的数字+字母'));
                 }else {
                     callback();
                 }
             };
+            //验证重复密码
+            let validatePasswords = (rule, value, callback) => {
+                //如果模块值为login,直接通过
+                if(model.value === 'login'){ callback(); }
+                //过滤后的数据
+                ruleForm.passwords = stripscript(value);
+                value =ruleForm.passwords;
+
+                if (value === '') {
+                    callback(new Error('请再次输入密码'));
+                }else if(value != ruleForm.password){
+                    return callback(new Error('重复密码不正确'));
+                }else {
+                    callback();
+                }
+            };
             //验证验证码
-            var validateCode = (rule, value, callback) => {
-                let reg = /^[a-zA-Z0-9]{6}$/;
+            let validateCode = (rule, value, callback) => {
                 if (value === '') {
                     callback(new Error('请输入验证码'));
-                }else if(!reg.test(value)){
+                }else if(validateVCode(value)){
                     return callback(new Error('验证码格式有误'));
                 }else {
                     callback();
                 }
             };
-            return {
-                menuTab:[
-                    {txt:'登录',current:true},
-                    {txt:'注册',current:false}
+
+            /**
+             * reactive 声明的数据是对象类型
+             * ref 声明的数据是基础类型
+             **/ 
+
+            const menuTab = reactive([
+                {txt:'登录',current:true ,type:'login'},
+                {txt:'注册',current:false,type:'register'}
+            ])
+
+            //模块值
+            const model = ref('login');
+            //表单绑定数据
+            const ruleForm = reactive({
+                username: '',
+                password: '',
+                passwords: '',
+                code: ''
+            })
+            //表单验证
+            const rules = reactive({
+                username: [
+                    { validator: validateUsername, trigger: 'blur' }
                 ],
-                isActive:true,
-                ruleForm: {
-                    username: '',
-                    password: '',
-                    code: ''
-                },
-                rules: {
-                    username: [
-                        { validator: validateUsername, trigger: 'blur' }
-                    ],
-                    password: [
-                        { validator: validatePassword, trigger: 'blur' }
-                    ],
-                    code: [
-                        { validator: validateCode, trigger: 'blur' }
-                    ]
-                }
-            };
-        },
-        methods: {
-            toggleMenu(data){
-                this.menuTab.forEach((elem,index)=>{
+                password: [
+                    { validator: validatePassword, trigger: 'blur' }
+                ],
+                passwords: [
+                    { validator: validatePasswords, trigger: 'blur' }
+                ],
+                code: [
+                    { validator: validateCode, trigger: 'blur' }
+                ]
+            })
+
+            // 声明函数
+            const toggleMenu = (data =>{
+                menuTab.forEach((elem,index)=>{
                     elem.current = false;
                 })
                 //高光
                 data.current = true;
-            },
-            submitForm(formName) {
-                this.$refs[formName].validate((valid) => {
-                if (valid) {
-                    alert('submit!');
-                } else {
-                    console.log('error submit!!');
-                    return false;
-                }
-            });
-            },
-            resetForm(formName) {
-                this.$refs[formName].resetFields();
+                //修改模块值
+                model.value = data.type;
+            })
+            const submitForm = (formName => {
+                refs[formName].validate((valid) => {
+                    if (valid) {
+                        alert('submit!');
+                    } else {
+                        console.log('error submit!!');
+                        return false;
+                    }
+                });
+            })
+            const resetForm = (formName => {
+                refs[formName].resetFields();
+            })
+
+            //组件挂载完成、内容更新或者解除挂载时 (这就对应了生命周期钩子)
+            onMounted(() => {
+                console.log('component is mounted!')
+            })
+
+            return{
+                menuTab,
+                model,
+                ruleForm,
+                rules,
+                toggleMenu,
+                submitForm,
+                resetForm
             }
         }
     }
